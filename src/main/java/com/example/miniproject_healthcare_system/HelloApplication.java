7e6d1597425ffd3e06754b2a9a4d1f9bcf0e99b1
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-09-02 15:49:05 +0530

    made some changes

--- src/main/java/com/example/miniproject_healthcare_system/HelloApplication.java
+++ src/main/java/com/example/miniproject_healthcare_system/HelloApplication.java
@@ -4,14 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("BaseScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 450);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.DECORATED);
         stage.setTitle("Health Buddy!");
         stage.setScene(scene);
         stage.show();
