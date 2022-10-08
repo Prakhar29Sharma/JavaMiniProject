7e0f960815ef38a46c2b3bca201cf6192b94db06
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-10-08 22:20:37 +0530

    added date, time and username display to every scene

--- src/main/java/com/example/miniproject_healthcare_system/DoctorController.java
+++ src/main/java/com/example/miniproject_healthcare_system/DoctorController.java
@@ -1,19 +1,26 @@
 package com.example.miniproject_healthcare_system;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class DoctorController implements Initializable {
@@ -41,10 +48,24 @@ public class DoctorController implements Initializable {
     @FXML
     private TableColumn<doctors, String> specialization;
 
+    @FXML
+    Label dateLabel;
+    @FXML
+    Label timeLabel;
+    @FXML
+    Label userLabel;
+
+    Timeline timeline;
 
     ObservableList<doctors> listM;
 
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            time();
+            userLabel.setText("Hello, " + DashboardController.getUsername());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         id.setCellValueFactory(new PropertyValueFactory<doctors, Integer>("id"));
         fname.setCellValueFactory(new PropertyValueFactory<doctors, String>("fname"));
         lname.setCellValueFactory(new PropertyValueFactory<doctors, String>("lname"));
@@ -56,11 +77,27 @@ public class DoctorController implements Initializable {
         doctorTable.setItems(listM);
     }
 
+    public void time() {
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            LocalDateTime date1 = LocalDateTime.now();
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String formatDate = date1.format(format);
+            DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formatTime1 = date1.format(formatTime);
+            dateLabel.setText(formatDate);
+            timeLabel.setText(formatTime1);
+            System.out.println("Doctor Date and Time");
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
 
     private Stage stage;
     private Scene scene;
 
     public void switchToLogin(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -69,6 +106,7 @@ public class DoctorController implements Initializable {
     }
 
     public void switchToDash(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -85,6 +123,7 @@ public class DoctorController implements Initializable {
     }
 
     public void switchToPatientRec(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("pat_recs.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -93,6 +132,7 @@ public class DoctorController implements Initializable {
     }
 
     public void switchToAppointments(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("appointments.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -101,6 +141,7 @@ public class DoctorController implements Initializable {
     }
 
     public void switchToSlots(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("slots.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -109,6 +150,7 @@ public class DoctorController implements Initializable {
     }
 
     public void switchToAddDoc(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("add_doc.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
--- src/main/java/com/example/miniproject_healthcare_system/SlotsController.java
+++ src/main/java/com/example/miniproject_healthcare_system/SlotsController.java
@@ -1,7 +1,6 @@
 package com.example.miniproject_healthcare_system;
 
 import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -10,51 +9,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.net.URL;
-import java.text.DateFormat;
-import java.time.*;
-import java.time.format.*;
-
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
-public class DashboardController implements Initializable {
-
+public class SlotsController implements Initializable {
     private Stage stage;
     private Scene scene;
 
     @FXML
     Label timeLabel;
+
     @FXML
     Label dateLabel;
+
 
     @FXML
     Label userLabel;
 
-    static String username;
-
     Timeline timeline;
-
-    public  void setUser(){
-        String greetUser = "Hello, " + username;
-        userLabel.setText(greetUser);
-    }
-
-    public static void setUsername(String usr){
-        username = usr;
-    }
-
-    public static String getUsername() {
-        return username;
-    }
 
     public void switchToLogin(ActionEvent event) throws IOException {
         timeline.stop();
@@ -66,6 +45,7 @@ public class DashboardController implements Initializable {
     }
 
     public void switchToDash(ActionEvent event) throws IOException {
+        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
@@ -101,15 +81,12 @@ public class DashboardController implements Initializable {
     }
 
     public void switchToSlots(ActionEvent event) throws IOException {
-        timeline.stop();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("slots.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
-
-
 
     public void time() {
          timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
@@ -120,7 +97,7 @@ public class DashboardController implements Initializable {
             String formatTime1 = date1.format(formatTime);
             dateLabel.setText(formatDate);
             timeLabel.setText(formatTime1);
-            System.out.println("Dashboard Date and Time");
+             System.out.println("Slots Date and Time");
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
@@ -128,21 +105,13 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //time();
+
         try {
-            /*LocalDate date = LocalDate.now();
-            String formattedDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-            dateLabel = new Label(formattedDate);
-            dateLabel.setText(formattedDate);
-             */
             time();
-            setUser();
+            userLabel.setText(DashboardController.getUsername());
         } catch (Exception e) {
             System.out.println(e);
         }
+
     }
-
-
-
-
 }
