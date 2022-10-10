6e9ed27de50442aaaaea3c570000a3bc8984d49a
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-10-10 15:15:52 +0530

    added patients class and patientRecController

--- src/main/java/com/example/miniproject_healthcare_system/PatientRegController.java
+++ src/main/java/com/example/miniproject_healthcare_system/PatientRegController.java
@@ -1,9 +1,14 @@
 package com.example.miniproject_healthcare_system;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +17,34 @@ public class PatientRegController {
 
     private Stage stage;
     private Scene scene;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField cityArea;
+
+    @FXML
+    private Label confirmationLabel;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField fname;
+
+    @FXML
+    private ChoiceBox<String> genderChoice;
+
+    @FXML
+    private TextField lname;
+
+    @FXML
+    private TextField phno;
+
+    @FXML
+    private Button registerButton;
+
 
     public void switchToPatientRec(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("pat_recs.fxml"));
